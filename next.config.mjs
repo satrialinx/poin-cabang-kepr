96072a850fcd7bbdf49384fe2f2133ebac09d1f7
satrialinx <satrialinx@gmail.com>
2026-02-26 00:04:10 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
   },
 }
-
+ 
 export default nextConfig
+ 
